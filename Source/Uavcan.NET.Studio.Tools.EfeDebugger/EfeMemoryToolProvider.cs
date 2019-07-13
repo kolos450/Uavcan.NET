8bea36c730a86a5d1b211da193d43383e6915ec4
kr <none@none>
2019-07-13 17:01:13 +0300

    EFE Debugger in progress.

---
+++ Source/Uavcan.NET.Studio.Tools.EfeDebugger/EfeMemoryToolProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Uavcan.NET.Studio.Tools.EfeDebugger
+{
+    [Export(typeof(IUavcanStudioToolProvider))]
+    sealed class EfeMemoryToolProvider : IUavcanStudioToolProvider
+    {
+        public string ToolTitle => "EFE Memory";
+
+        public UIElement CreateUIElement(UavcanInstance uavcan)
+        {
+            return new EfeMemoryControl(uavcan);
+        }
+    }
+}

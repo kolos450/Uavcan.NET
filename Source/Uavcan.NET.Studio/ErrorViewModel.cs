185d4b592918b8a61468ae84334ee9b10b9e0d62
RKirill <none@none>
2019-10-17 15:07:25 +0300

    Error window was implemented.

---
+++ Source/Uavcan.NET.Studio/ErrorViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uavcan.NET.Studio
+{
+    sealed class ErrorViewModel : ReactiveObject
+    {
+        string _description;
+        public string Description { get => _description; set => this.RaiseAndSetIfChanged(ref _description, value); }
+
+        string _details;
+        public string Details { get => _details; set => this.RaiseAndSetIfChanged(ref _details, value); }
+
+        bool _detailsVisible;
+        public bool DetailsVisisble { get => _detailsVisible; set => this.RaiseAndSetIfChanged(ref _detailsVisible, value); }
+
+        public ReactiveCommand<Unit, Unit> ToggleDetailsVisibility { get; set; }
+
+        public ErrorViewModel()
+        {
+            ToggleDetailsVisibility = ReactiveCommand.Create(() =>
+            {
+                DetailsVisisble = !DetailsVisisble;
+            });
+        }
+    }
+}
---
+++ Source/Uavcan.NET.Studio/ErrorWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using MahApps.Metro.Controls;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Uavcan.NET.Studio
+{
+    /// <summary>
+    /// Interaction logic for ErrorWindow.xaml
+    /// </summary>
+    partial class ErrorWindow : MetroWindow, IViewFor<ErrorViewModel>
+    {
+        public ErrorWindow(ErrorViewModel viewModel)
+        {
+            InitializeComponent();
+
+            ViewModel = viewModel;
+
+            this.WhenActivated(d =>
+            {
+                this.OneWayBind(ViewModel,
+                    vm => vm.Description,
+                    v => v.tbDescription.Text)
+                    .DisposeWith(d);
+
+                this.OneWayBind(ViewModel,
+                    vm => vm.Details,
+                    v => v.tbDetails.Text)
+                    .DisposeWith(d);
+
+                this.OneWayBind(ViewModel,
+                    vm => vm.DetailsVisisble,
+                    v => v.tbDetails.Visibility)
+                    .DisposeWith(d);
+
+                this.BindCommand(ViewModel,
+                    vm => vm.ToggleDetailsVisibility,
+                    v => v.bToggleDetailsVisibility)
+                    .DisposeWith(d);
+            });
+        }
+
+        public ErrorViewModel ViewModel { get; set; }
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (ErrorViewModel)value;
+        }
+
+        public static void Show(string description, string details)
+        {
+            var vm = new ErrorViewModel
+            {
+                Description = description,
+                Details = details,
+            };
+
+            new ErrorWindow(vm).ShowDialog();
+        }
+    }
+}

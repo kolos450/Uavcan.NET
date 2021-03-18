fc14c418f094dc7169a99fc26d1a6ba95fbe50e5
kolos450 <kolos450@gmail.com>
2021-03-19 00:48:49 +0200

    Better crash dumps.

--- Source/Uavcan.NET.Studio/Program.cs
+++ Source/Uavcan.NET.Studio/Program.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Windows.Forms;
 using Uavcan.NET.IO.Can.Drivers.Slcan;
 
@@ -18,8 +19,17 @@ namespace Uavcan.NET.Studio
         static void Main()
         {
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += Application_ThreadException;
 
-            new Program().Run();
+            try
+            {
+                new Program().Run();
+            }
+            catch (Exception ex)
+            {
+                LogUnhandledException(ex);
+            }
         }
 
         [Import]
@@ -66,21 +76,56 @@ namespace Uavcan.NET.Studio
             }
         }
 
+        static StreamWriter _crashLogWriter;
+        static int _crashLogCounter = 0;
+
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             if (e.IsTerminating)
             {
-                const string FileName = "CRASH_LOG.txt";
+                LogUnhandledException(e.ExceptionObject);
+                Environment.Exit(1);
+            }
+        }
 
-                var filePath = Path.GetFullPath(FileName);
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            LogUnhandledException(e.Exception);
+            Environment.Exit(1);
+        }
+
+        private static void LogUnhandledException(object exceptionObject)
+        {
+            const string FileName = "CRASH_LOG.txt";
+
+            lock (typeof(Program))
+            {
+                bool disposeWriter = false;
                 try
                 {
-                    using (var tw = File.CreateText(filePath))
+                    if (_crashLogWriter is null)
                     {
-                        tw.WriteLine("***** Crash Dump *****");
-                        tw.WriteLine();
-                        tw.WriteLine(e.ExceptionObject?.ToString() ?? "NULL");
+                        disposeWriter = true;
+                        var filePath = Path.GetFullPath(FileName);
+
+                        if (_crashLogCounter++ == 0)
+                        {
+                            _crashLogWriter = File.CreateText(filePath);
+                            _crashLogWriter.WriteLine("***** Crash Dump *****");
+                            _crashLogWriter.WriteLine();
+                        }
+                        else
+                        {
+                            _crashLogWriter = File.AppendText(filePath);
+                            _crashLogWriter.WriteLine();
+                            _crashLogWriter.WriteLine("**********************");
+                            _crashLogWriter.WriteLine("**********************");
+                            _crashLogWriter.WriteLine("**********************");
+                            _crashLogWriter.WriteLine();
+                        }
                     }
+
+                    _crashLogWriter.WriteLine(CreateExceptioMessage(exceptionObject) ?? "NULL");
 
                     foreach (Form i in Application.OpenForms)
                         i.Close();
@@ -88,15 +133,72 @@ namespace Uavcan.NET.Studio
                 catch
                 {
                 }
-
-                MessageBox.Show(
-                    $"Unrecoverable error encountered.\n\n{FileName} file was created.",
-                    "Error",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Error);
-
-                Environment.Exit(1);
+                finally
+                {
+                    if (disposeWriter)
+                    {
+                        _crashLogWriter?.Dispose();
+                        _crashLogWriter = null;
+                    }
+                }
             }
+
+            MessageBox.Show(
+                $"Unrecoverable error encountered.\n\n{FileName} file was created.",
+                "Error",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+        }
+
+        private static string CreateExceptioMessage(object exceptionObject)
+        {
+            if (exceptionObject is null)
+                return null;
+
+            try
+            {
+                return exceptionObject.ToString();
+            }
+            catch (Exception ex)
+            {
+                var sb = new StringBuilder();
+                sb.Append("Cannot get ");
+                sb.Append(exceptionObject.GetType().FullName);
+                sb.AppendLine(" message:");
+                sb.AppendLine(ex.ToString());
+
+                try
+                {
+                    bool subsectionTitleWritten = false;
+                    foreach (var nested in EnumerateNestedExceptions(exceptionObject))
+                    {
+                        if (!subsectionTitleWritten)
+                        {
+                            sb.AppendLine();
+                            sb.AppendLine("Nested exceptions:");
+                            subsectionTitleWritten = true;
+                        }
+
+                        sb.AppendLine(nested.ToString());
+                    }
+                }
+                catch { }
+
+                return sb.ToString();
+            }
+        }
+
+        private static IEnumerable<Exception> EnumerateNestedExceptions(object exceptionObject)
+        {
+            switch (exceptionObject)
+            {
+                case CompositionException compositionException:
+                    return compositionException.RootCauses;
+                case AggregateException aggregateException:
+                    return aggregateException.InnerExceptions;
+            }
+
+            return Enumerable.Empty<Exception>();
         }
     }
 }
